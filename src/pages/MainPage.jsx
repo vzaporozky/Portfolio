1a9e3fdb8a6f5bcae9743d80fe99fbd94afa7d53
vzaporozky <zaporozhskiy2002@gmail.com>
2023-02-27 20:45:27 +0200

    ChatGPT clone update

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -12,6 +12,18 @@ const MainPage = () => {
             <h1>Applications</h1>
             <div className="home_container">
                 <LinkToApp 
+                    nameApp='ChatGPT Clone'
+                    to="/ChatGPT"
+                    table='2'
+                    firstTable={['html','CSS','JavaScript', 'React']}
+                    secondTable={['React(updated)']}
+                    src='./images/ChatGPT_main.png'
+                    img1='./images/ChatGPT_1.png'
+                    img2='./images/ChatGPT_2.png'
+                    img3='./images/ChatGPT_main.png'
+                    description={['This is a simple chat that uses the OpenAI API. The user can input text messages and receive response messages processed by the OpenAI API.']}
+                />
+                <LinkToApp 
                     nameApp='Color Game'
                     to="/colorgame"
                     table='2'
@@ -36,20 +48,7 @@ const MainPage = () => {
                     img3='./images/Hangman_3.png'
                     description={['In this game, you need to try to find the right word.', 
                         'You can ask for a hint.','P.S. not finished...(']}
-                />
-                <LinkToApp 
-                    nameApp='ChatGPT'
-                    to="/ChatGPT"
-                    table='2'
-                    firstTable={['html','CSS','JavaScript', 'React']}
-                    secondTable={['React(updated)']}
-                    src='./images/ColorGame_main.png'
-                    img1='./images/ColorGame_1.png'
-                    img2='./images/ColorGame_2.png'
-                    img3='./images/ColorGame_3.png'
-                    description={['In this game you can find right color. If color its corect, this color will hide, and you will be looking for corect color', 
-                        'This game is needed to pass the time.']}
-                    toolTip = 'left'
+                        toolTip = 'left'
                 />
                 
             </div>
--- src/pages/apps/ChatGPT/ChatGPT.jsx
+++ src/pages/apps/ChatGPT/ChatGPT.jsx
@@ -1,42 +1,42 @@
 import React, {useState} from "react";
 import { Configuration, OpenAIApi } from "openai";
 import key from "./keyAPI.jsx";
+import keyAPI_example from "./keyAPI_example.jsx";
 import { arrayItems } from './index'
 import classes from './ChatGPT.module.css'
 
 const ChatGPT = () =>{
     const configuration = new Configuration({
-        apiKey: key,
+        // apiKey: key, // <- <-<- add api key to file keyAPI.jsx and uncommit it
+        apiKey: keyAPI_example, // commit it
     });
     const openai = new OpenAIApi(configuration);
     const [option, setOption] = useState(arrayItems[0].option);
-    // const [option, setOption] = useState({});
     const [input, setInput] = useState('');
+    const [prevTextOfBot, setPrevTextOfBot] = useState('');
     const [result, setResult] = useState([]);
-    // const [result, setResult] = useState('');
-
-    // const selectOption = (option) => {
-    //     setOption(option);
-    // };
 
     const doStuff = async () => {
+        console.log(prevTextOfBot)
         setResult([{writer: "user", value:input}, ...result, ]);
 
-        let object = { ...option, prompt: input };
+        let object = '';
+        let response = '';
 
-        const response = await openai.createCompletion(object);
+        if(input === 'continue' || input === 'продолжай' || input === 'продовжуй') {
+            let object = { ...option, prompt: input + ' ' + prevTextOfBot};
+            response = await openai.createCompletion(object);
+        }else{
+            object = { ...option, prompt: input };
+            response = await openai.createCompletion(object);
+        }
 
-        // setResult([...result, {writer: "user", value:input}]);
-        // setResult([...result, {writer: "user", value:input},{writer: "bot", value:response.data.choices[0].text}])
+        
+
+        setPrevTextOfBot(response.data.choices[0].text)
         setResult([{writer: "bot", value:response.data.choices[0].text}, {writer: "user", value:input},  ...result, ])
         setInput('');
-        // console.log(result)
-        // setResult(response.data.choices[0].text);
     };
-
-    // const userSetInResult = (event) =>{
-    //     setResult([...result, {writer: "user", value:input}]);
-    // }
 
     return (
         <div className={classes.container}>
@@ -44,13 +44,13 @@ const ChatGPT = () =>{
                 <input
                     type="text"
                     value={input}
+                    placeholder="say 'Hello'"
                     className={classes.input}
                     onChange={(e) => setInput(e.target.value)}
                 />
                 <button className={classes.action_btn} onClick={doStuff}><img src="./icon_send.png" alt="" style={{height:'20px'}} /></button>
             </div>
 
-            {/* <h3 className="result-text">{result.length > 0 ? result : ""}</h3> */}
             <div className={classes.result_block}>{result.length > 0 ? result.map((el, i) =>{
                 if(el.writer === 'bot'){
                     return <div key={i} className={classes.text_bot + ' ' + classes.result_text}>{el.value}<div/></div>
