f187d1561a1a918afd5b22e411682e8068f18005
vzaporozky <zaporozhskiy2002@gmail.com>
2023-02-27 19:49:24 +0200

    ChatGPT last

--- src/pages/apps/ChatGPT/ChatGPT.jsx
+++ src/pages/apps/ChatGPT/ChatGPT.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { Configuration, OpenAIApi } from "openai";
-import key from "./keyAPI";
+import key from "./keyAPI.jsx";
 import { arrayItems } from './index'
 import classes from './ChatGPT.module.css'
 
@@ -10,36 +10,54 @@ const ChatGPT = () =>{
     });
     const openai = new OpenAIApi(configuration);
     const [option, setOption] = useState(arrayItems[0].option);
-    const [input, setInput] = useState("");
-    const [result, setResult] = useState([{writer: "user", value:'hello'}, {writer: 'bot', value:'hi'}]);
+    // const [option, setOption] = useState({});
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState([]);
+    // const [result, setResult] = useState('');
 
     // const selectOption = (option) => {
     //     setOption(option);
     // };
 
     const doStuff = async () => {
+        setResult([{writer: "user", value:input}, ...result, ]);
+
         let object = { ...option, prompt: input };
 
         const response = await openai.createCompletion(object);
 
-        setResult(response.data.choices[0].text);
+        // setResult([...result, {writer: "user", value:input}]);
+        // setResult([...result, {writer: "user", value:input},{writer: "bot", value:response.data.choices[0].text}])
+        setResult([{writer: "bot", value:response.data.choices[0].text}, {writer: "user", value:input},  ...result, ])
+        setInput('');
+        // console.log(result)
+        // setResult(response.data.choices[0].text);
     };
+
+    // const userSetInResult = (event) =>{
+    //     setResult([...result, {writer: "user", value:input}]);
+    // }
 
     return (
         <div className={classes.container}>
-            <textarea
-                className={classes.text_area}
-                cols={55}
-                rows={10}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <button className={classes.action_btn} onClick={doStuff}>
-                DO YOU STUFF!
-            </button>
+            <div className={classes.request}>
+                <input
+                    type="text"
+                    value={input}
+                    className={classes.input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button className={classes.action_btn} onClick={doStuff}><img src="./icon_send.png" alt="" style={{height:'20px'}} /></button>
+            </div>
 
-            <h3 className={classes.result_text}>{result.length > 0 ? result.map(el =>{
-                return <span>{el.value}</span>
-            }) : ""}</h3>
+            {/* <h3 className="result-text">{result.length > 0 ? result : ""}</h3> */}
+            <div className={classes.result_block}>{result.length > 0 ? result.map((el, i) =>{
+                if(el.writer === 'bot'){
+                    return <div key={i} className={classes.text_bot + ' ' + classes.result_text}>{el.value}<div/></div>
+                }else if(el.writer === 'user'){
+                    return <div key={i} className={classes.text_user + ' ' + classes.result_text}>{el.value}<div/></div>
+                }
+            }) : ""}</div>
         </div>
     );
 }
--- src/pages/apps/ChatGPT/index.jsx
+++ src/pages/apps/ChatGPT/index.jsx
@@ -5,7 +5,7 @@ export const arrayItems = [
       option: {
         model: "text-davinci-003",
         temperature: 0,
-        max_tokens: 100,
+        max_tokens: 1000,
         top_p: 1,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
