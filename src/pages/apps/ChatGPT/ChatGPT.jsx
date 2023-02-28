1f0f7cf5fed0b52055a7f1957ca2f9a4ea986e99
vzaporozky <zaporozhskiy2002@gmail.com>
2023-02-28 15:21:12 +0200

    chat gpt push

--- src/pages/apps/ChatGPT/ChatGPT.jsx
+++ src/pages/apps/ChatGPT/ChatGPT.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Configuration, OpenAIApi } from "openai";
 import key from "./keyAPI.jsx";
 import keyAPI_example from "./keyAPI_example.jsx";
@@ -7,8 +7,8 @@ import classes from './ChatGPT.module.css'
 
 const ChatGPT = () =>{
     const configuration = new Configuration({
-        // apiKey: key, // <- <-<- add api key to file keyAPI.jsx and uncommit it
-        apiKey: keyAPI_example, // commit it
+        apiKey: key, // <- <- <- add api key to file keyAPI.jsx and uncommit it
+        // apiKey: keyAPI_example, // commit it
     });
     const openai = new OpenAIApi(configuration);
     const [option, setOption] = useState(arrayItems[0].option);
@@ -31,15 +31,37 @@ const ChatGPT = () =>{
             response = await openai.createCompletion(object);
         }
 
-        
-
         setPrevTextOfBot(response.data.choices[0].text)
         setResult([{writer: "bot", value:response.data.choices[0].text}, {writer: "user", value:input},  ...result, ])
         setInput('');
     };
 
+    const showInfo = () =>{
+        if(localStorage.getItem('info')){
+            localStorage.removeItem('info')
+            (localStorage.getItem('info_checked') ? '' : localStorage.setItem('info_checked', true) )
+        }else{
+            localStorage.setItem('info', true)
+        }
+    }
+
+    const handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            doStuff()
+        }
+      }
+
     return (
         <div className={classes.container}>
+            {!key && 
+            // {!keyAPI_example && 
+                <button className={classes.info_block} onClick={showInfo}>
+                    <img src="./arrow_info.png" alt="" className={classes.info_arrow} />
+                    <div className={classes.info_text}>
+                        You need to add APIkey
+                    </div>
+                </button>
+            }
             <div className={classes.request}>
                 <input
                     type="text"
@@ -47,6 +69,7 @@ const ChatGPT = () =>{
                     placeholder="say 'Hello'"
                     className={classes.input}
                     onChange={(e) => setInput(e.target.value)}
+                    onKeyPress={handleKeyPress}
                 />
                 <button className={classes.action_btn} onClick={doStuff}><img src="./icon_send.png" alt="" style={{height:'20px'}} /></button>
             </div>
